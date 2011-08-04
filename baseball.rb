0724498cceada9355747eff6a169083f9f4c216d
yous <chacha@kaist.ac.kr>
2011-08-04 03:03:30 -0700

    각 팀의 정보 출력

--- baseball.rb
+++ baseball.rb
@@ -4,4 +4,6 @@ require 'rubygems'
 require 'mechanize'
 
 a = Mechanize.new.get 'http://kbodata.news.naver.com/m_rank/rank_team.asp'
-puts a.body
+a.search('//table[@class="table_board2"]/tbody/tr').each {|v|
+  puts v.text
+}
