b7be9fd37f018819d3a3d85c9a928f3e55af227a
yous <chacha@kaist.ac.kr>
2011-08-04 03:10:14 -0700

    td를 이용해 정보 출력

--- baseball.rb
+++ baseball.rb
@@ -5,5 +5,5 @@ require 'mechanize'
 
 a = Mechanize.new.get 'http://kbodata.news.naver.com/m_rank/rank_team.asp'
 a.search('//table[@class="table_board2"]/tbody/tr').each {|v|
-  puts v.text
+  puts v.search('td').text
 }
