74baef84210a4939735bbf7695961d36349a57fb
yous <chacha@kaist.ac.kr>
2011-08-04 03:20:55 -0700

    야구 순위 및 최근 성적 출력

--- baseball.rb
+++ baseball.rb
@@ -6,5 +6,6 @@ require 'mechanize'
 a = Mechanize.new.get 'http://kbodata.news.naver.com/m_rank/rank_team.asp'
 a.search('//table[@class="table_board2"]/tbody/tr').each {|v|
   p = v.search('td')
-  puts "#{p[0].text} - #{p[1].text} - #{p[-1].text}"
+  p[-1].text =~ /(\d+)승\s*(\d+)패\s*(\d+)무/
+  puts "#{p[0].text} - #{p[1].text} - #{$1} - #{$2} - #{$3}"
 }
