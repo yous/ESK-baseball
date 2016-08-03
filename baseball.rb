44d23ed78f68b292246b1f7457f142d113346214
Chayoung You <yousbe@gmail.com>
2016-08-03 15:07:42 +0900

    Rewrite for new URL (Fix #1)

--- baseball.rb
+++ baseball.rb
@@ -1,11 +1,15 @@
 #!/usr/bin/env ruby
-
-require 'rubygems'
 require 'mechanize'
 
-a = Mechanize.new.get 'http://kbodata.news.naver.com/m_rank/rank_team.asp'
-a.search('//table[@class="table_board2"]/tbody/tr').each {|v|
-  p = v.search('td')
-  p[-1].text =~ /(\d+)승\s*(\d+)패\s*(\d+)무/
-  puts "#{p[0].text} - #{p[1].text} - #{$1} - #{$2} - #{$3}"
-}
+URL = 'http://sports.news.naver.com/kbaseball/record/index.nhn'
+a = Mechanize.new
+
+a.get(URL) do |page|
+  page.search('//tbody[@id="regularTeamRecordList_table"]/tr').each do |tr|
+    rank = tr.at('th').text
+    team = tr.at('td[@class="tm"]').text.strip
+    record = tr.at('td[@class="last"]').text
+    match = /(\d+)승-(\d+)패-(\d+)무/.match(record)
+    puts "#{rank} - #{team} - #{match[1]}W - #{match[2]}L - #{match[3]}T"
+  end
+end
